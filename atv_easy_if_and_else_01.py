27e882573bcca70a3c2324a86032d85b51f9eb9e
Danyelmariomoura <163203661+Danyelmariomoura@users.noreply.github.com>
2024-03-12 18:06:18 -0300

    atv 01

---
+++ atv_easy_if_and_else_01.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the First number: "))
+num2 = float(input("Enter the Second numbers: "))
+
+if num1 > num2:
+    print(f"The number {num1} is greater the {num2}.")
+else:
+    print(f"The number {num2} is greater the {num1}")
